b61323a1530d6d3ea2c6f1b5158a41b266f9f1f8
Nawshadss <hossainmdnawshad@gmail.com>
2024-01-27 15:44:55 +0600

    grade done

---
+++ grade.js
@@ -0,0 +1,15 @@
+// task 3
+
+grade = 59;
+
+if (grade >= 90) {
+  console.log("youve gotten A+");
+} else if (grade >= 80 && grade < 90) {
+  console.log("youve gotten B");
+} else if (grade >= 70 && grade < 80) {
+  console.log("You've gotten C");
+} else if (grade >= 60 && grade < 70) {
+  console.log("youve gotten D");
+} else if (grade <= 59) {
+  console.log("Youved fail");
+}
