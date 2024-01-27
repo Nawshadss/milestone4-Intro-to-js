3239fe9078a594c4287dcf4496074041893b8893
Nawshadss <hossainmdnawshad@gmail.com>
2024-01-27 15:36:30 +0600

    bmi calc done

--- multipleComp.js
+++ multipleComp.js
@@ -4,3 +4,18 @@ if (burger >= 500) {
 } else {
   console.log("pay for 30tk driks");
 }
+
+// task 2---------------------
+
+Bmi = 85 / 1.9 ** 2;
+console.log(Bmi);
+
+if (Bmi < 18.5) {
+  console.log("youre under weight");
+} else if (Bmi > 18.5 && Bmi < 24.9) {
+  console.log("youre weight is norlmal");
+} else if (Bmi >= 25 && Bmi < 29.9) {
+  console.log("your are over weight ");
+} else {
+  console.log("youre a fatso");
+}
