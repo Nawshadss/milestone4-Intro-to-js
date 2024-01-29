58a072a9e2230898749fa2d4cea6b5ad6f7e5123
Nawshadss <hossainmdnawshad@gmail.com>
2024-01-29 20:54:23 +0600

    done

--- whileLoop.js
+++ whileLoop.js
@@ -4,3 +4,15 @@ while (num < 9) {
   num++;
 }
 console.log("fsdf ");
+
+const arr = [3, 4, 54, 67, 5, 4, 78];
+let arLenth = arr.length;
+let count = 0;
+let sum;
+while (count < arLenth) {
+  sum = arr[count];
+  total = sum + sum;
+
+  count++;
+}
+console.log(total);
